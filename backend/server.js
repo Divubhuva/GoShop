45d486f64418ac72bda90b27690ea2f7e207b5b9
Divubhuva <divubhuva@gmail.com>
2020-09-26 23:00:05 -0400

    Testing server.js

--- backend/server.js
+++ backend/server.js
@@ -8,37 +8,32 @@ import productRoute from './routes/productRoute';
 import orderRoute  from './routes/orderRoute'
 
 
-const mongodbUrl = config.MONGODB_URL;
-console.log(mongodbUrl);
-mongoose.connect(mongodbUrl, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true
-}).catch(error => console.log(error.reason));
+// const mongodbUrl = config.MONGODB_URL;
+// console.log(mongodbUrl);
+// mongoose.connect(mongodbUrl, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useCreateIndex: true
+// }).catch(error => console.log(error.reason));
 
 
 const app = express();
-app.use(bodyParser.json());
-
-app.use("/api/users", userRoute);
-app.use("/api/products", productRoute);
-app.use("/api/orders", orderRoute);
+// app.use(bodyParser.json());
+// app.use("/api/users", userRoute);
+// app.use("/api/products", productRoute);
+// app.use("/api/orders", orderRoute);
+app.get("/api/users", (req, res) => {
+  res.send("user is calling")
+})
+app.get("/api/products", (req, res) => {
+  res.send("product is calling")
+})
+app.get("/api/orders", (req, res) => {
+  res.send("order is calling")
+})
 app.get("/api/config/paypal", (req, res) => {
   res.send(config.PAYPAL_CLIENT_ID)
 
 })
-
-// app.get("/api/products/:id", (req, res) => {
-//   const productId = req.params.id;
-//   const product = data.products.find(x => x._id === productId);
-//   if (product)
-//     res.send(product);
-//   else
-//     res.status(404).send({ msg: "Product Not Found." })
-// });
-
-// app.get("/api/products", (req, res) => {
-//   res.send(data.products);
-// });
 
 app.listen(5000, () => { console.log("Server started at http://localhost:5000") });
