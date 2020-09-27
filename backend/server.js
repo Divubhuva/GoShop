594ab5e5bbb0acae4cfc363ab9cadbe6320db8b2
Divubhuva <divubhuva@gmail.com>
2020-09-26 23:08:57 -0400

    testing port

--- backend/server.js
+++ backend/server.js
@@ -35,5 +35,5 @@ app.get("/api/config/paypal", (req, res) => {
   res.send(config.PAYPAL_CLIENT_ID)
 
 })
-
-app.listen(5000, () => { console.log("Server started at http://localhost:5000") });
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => { console.log("Server started at http://localhost:5000") });
