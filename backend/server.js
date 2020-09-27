6d49cb80f94a4db3a183338b7b95b01650beb648
Divubhuva <divubhuva@gmail.com>
2020-09-26 23:30:00 -0400

    PORT error solved.

--- backend/server.js
+++ backend/server.js
@@ -8,35 +8,32 @@ import productRoute from './routes/productRoute';
 import orderRoute  from './routes/orderRoute'
 
 
-// const mongodbUrl = config.MONGODB_URL;
-// console.log(mongodbUrl);
-// mongoose.connect(mongodbUrl, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-//   useCreateIndex: true
-// }).catch(error => console.log(error.reason));
+const mongodbUrl = config.MONGODB_URL;
+console.log(mongodbUrl);
+mongoose.connect(mongodbUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+}).catch(error => console.log(error.reason));
 
 
 const app = express();
-app.get("/", (req, res) => {
-  res.send("/ is calling")
-})
-// app.use(bodyParser.json());
-// app.use("/api/users", userRoute);
-// app.use("/api/products", productRoute);
-// app.use("/api/orders", orderRoute);
-app.get("/api/users", (req, res) => {
-  res.send("user is calling")
-})
-app.get("/api/products", (req, res) => {
-  res.send("product is calling")
-})
-app.get("/api/orders", (req, res) => {
-  res.send("order is calling")
-})
+app.use(bodyParser.json());
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/orders", orderRoute);
+// app.get("/api/users", (req, res) => {
+//   res.send("user is calling")
+// })
+// app.get("/api/products", (req, res) => {
+//   res.send("product is calling")
+// })
+// app.get("/api/orders", (req, res) => {
+//   res.send("order is calling")
+// })
 app.get("/api/config/paypal", (req, res) => {
   res.send(config.PAYPAL_CLIENT_ID)
 
 })
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => { console.log("Server started at http://localhost:5000") });
+app.listen(5000, () => { console.log("Server started at http://localhost:5000") });
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,93 +17,90 @@ import OrdersScreen from './Screens/OrdersScreen'
 import {useSelector} from 'react-redux'
 
 function App() {
-    return <div>
-        Yash
-    </div>
 
-//     const userSignin = useSelector((state) => state.userSignin)
-//     const {userInfo} = userSignin;
+    const userSignin = useSelector((state) => state.userSignin)
+    const {userInfo} = userSignin;
 
 
-//   const openMenu = () => {
-//     document.querySelector(".sidebar").classList.add("open")
-//   };
+  const openMenu = () => {
+    document.querySelector(".sidebar").classList.add("open")
+  };
 
-//   const closeMenu = () => {
-//     document.querySelector(".sidebar").classList.remove("open")
-//   };
-//   return (
-//     <BrowserRouter>
-//     <div className="grid-container">
-//             <header className="header">
-//                 <div className="brand">
-//                     <button onClick={openMenu}>
-//                         &#9776;
-//                     </button>
-//                     <Link to="/" >GoShop</Link>
-//                 </div>
-//                 <div className="header-links">
-//                 <Link to="/cart"> Cart</Link>
-//                     {
-//                         userInfo ? <Link to="/profile">{userInfo.name}</Link> :
-//                         <Link to="/signin"> Sign In</Link>
-//                     }
+  const closeMenu = () => {
+    document.querySelector(".sidebar").classList.remove("open")
+  };
+  return (
+    <BrowserRouter>
+    <div className="grid-container">
+            <header className="header">
+                <div className="brand">
+                    <button onClick={openMenu}>
+                        &#9776;
+                    </button>
+                    <Link to="/" >GoShop</Link>
+                </div>
+                <div className="header-links">
+                <Link to="/cart"> Cart</Link>
+                    {
+                        userInfo ? <Link to="/profile">{userInfo.name}</Link> :
+                        <Link to="/signin"> Sign In</Link>
+                    }
 
-//                     {userInfo && userInfo.isAdmin && (
-//                         <div className="dropdown">
-//                         <a href="#"  >Admin</a>
-//                         <ul className="dropdown-content">
-//                             <li>
-//                                 <Link to="/orders">Orders</Link>
-//                                 <Link to="/products">Products</Link>
-//                             </li>
-//                         </ul>
-//                         </div>
-//                     )}
+                    {userInfo && userInfo.isAdmin && (
+                        <div className="dropdown">
+                        <a href="#"  >Admin</a>
+                        <ul className="dropdown-content">
+                            <li>
+                                <Link to="/orders">Orders</Link>
+                                <Link to="/products">Products</Link>
+                            </li>
+                        </ul>
+                        </div>
+                    )}
 
-//                 </div>
-//             </header>
-//             <aside className="sidebar">
-//                 <h3>Shopping Categories</h3>
-//                 <button className="sidebar-close-button" onClick={closeMenu}>x</button>
-//                 <ul className="categories">
-//                     <li>
-//                         <Link to="/category/Office">Office Dresses</Link>
-//                     </li>
-//                     <li>
-//                         <Link to="/category/Summer">Summer Dresses</Link>
-//                     </li>
-//                     <li>
-//                         <Link to="/category/Party">Party Dresses</Link>
-//                     </li>
-//                     <li>
-//                         <Link to="/category/Plus">Plus Size Dresses</Link>
-//                     </li>
-//                 </ul>
-//             </aside>
-//             <main className="main">
-//                 <div className="content">
-//                     <Route path="/orders" component={OrdersScreen} />
-//                     <Route path="/profile" component={ProfileScreen} />       
-//                     <Route path="/order/:id" component={OrderScreen} />
-//                     <Route path="/products" component={ProductsScreen} />
-//                     <Route path="/shipping" component={ShippingScreen} />
-//                     <Route path="/payment" component={PaymentScreen} />
-//                     <Route path="/placeorder" component={placeOrderScreen} />
-//                     <Route path="/signin" component={SigninScreen} />
-//                     <Route path="/register" component={RegisterScreen} />
-//                     <Route path="/product/:id" component={ProductScreen} />
-//                     <Route path="/cart/:id?" component={CartScreen} />
-//                     <Route path="/category/:id" component={HomeScreen} />
-//                     <Route path="/" exact={true} component={HomeScreen} />
-//                 </div>
-//             </main>
-//             <footer className="footer">
-//                 All right reserved.
-//             </footer>
-//         </div>
-//         </BrowserRouter>
-//     );
+                </div>
+            </header>
+            <aside className="sidebar">
+                <h3>Shopping Categories</h3>
+                <button className="sidebar-close-button" onClick={closeMenu}>x</button>
+                <ul className="categories">
+                    <li>
+                        <Link to="/category/Office">Office Dresses</Link>
+                    </li>
+                    <li>
+                        <Link to="/category/Summer">Summer Dresses</Link>
+                    </li>
+                    <li>
+                        <Link to="/category/Party">Party Dresses</Link>
+                    </li>
+                    <li>
+                        <Link to="/category/Plus">Plus Size Dresses</Link>
+                    </li>
+                </ul>
+            </aside>
+            <main className="main">
+                <div className="content">
+                    <Route path="/orders" component={OrdersScreen} />
+                    <Route path="/profile" component={ProfileScreen} />       
+                    <Route path="/order/:id" component={OrderScreen} />
+                    <Route path="/products" component={ProductsScreen} />
+                    <Route path="/shipping" component={ShippingScreen} />
+                    <Route path="/payment" component={PaymentScreen} />
+                    <Route path="/placeorder" component={placeOrderScreen} />
+                    <Route path="/signin" component={SigninScreen} />
+                    <Route path="/register" component={RegisterScreen} />
+                    <Route path="/product/:id" component={ProductScreen} />
+                    <Route path="/cart/:id?" component={CartScreen} />
+                    <Route path="/category/:id" component={HomeScreen} />
+                    <Route path="/" exact={true} component={HomeScreen} />
+                </div>
+            </main>
+            <footer className="footer">
+                All right reserved.
+            </footer>
+        </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
