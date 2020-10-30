445dda7c4db8123ffa71de8cb17faf8bfe4d3ce9
Divubhuva <divubhuva@gmail.com>
2020-10-30 01:42:51 -0400

    Loader is competed

--- frontend/src/components/LoadingComp.js
+++ frontend/src/components/LoadingComp.js
@@ -3,7 +3,10 @@ import React, { Component } from 'react'
 export default class LoadingComp extends Component {
     render() {
         return (
-            <div className ="loader"></div>
+            <div className="Loading">
+            <div className ="loader"> </div>
+            <div className = "LoadingText">Loading...</div>
+            </div>
         )
     }
 }
