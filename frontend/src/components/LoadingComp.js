1146fcc566674d10e21b5e628c8daad29b182823
Divubhuva <divubhuva@gmail.com>
2020-10-30 02:13:19 -0400

    Make Change in AboutScreen

--- frontend/src/components/LoadingComp.js
+++ frontend/src/components/LoadingComp.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class LoadingComp extends Component {
     render() {
         return (
-            <div className="Loading">
+            <div className="Center">
             <div className ="loader"> </div>
             <div className = "LoadingText">Loading...</div>
             </div>
